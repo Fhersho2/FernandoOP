0b0aaec5e05308013d71d6efdf014a3135a83b64
FernandoJOP <fherosoria@gmail.com>
2020-12-06 18:20:26 -0600

    1er commit

---
+++ MathGit/src/MathGit/MathGit.java
@@ -0,0 +1,12 @@
+
+package MathGit;
+import MathGit.Sumar;
+public class MathGit {
+
+    public static void main(String[] args) {
+        Sumar sumar = new Sumar();
+        int x = 10;
+        int y = 20;
+        System.out.println(sumar.suma(x,y));
+    }
+}
