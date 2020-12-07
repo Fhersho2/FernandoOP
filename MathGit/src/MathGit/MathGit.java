9ac99f01863598325c8f379823bec0f5478fa3b0
terckoer <prueba@gmail.com>
2020-12-06 18:18:09 -0600

    Funcionalidad resta

--- MathGit/src/MathGit/MathGit.java
+++ MathGit/src/MathGit/MathGit.java
@@ -4,7 +4,8 @@ package MathGit;
 public class MathGit {
 
     public static void main(String[] args) {
-        
+        Resta r1 = new Resta();
+        System.out.println(r1.resta(5, 2));
     }
     
 }
---
+++ MathGit/src/MathGit/Resta.java
@@ -0,0 +1,12 @@
+
+package MathGit;
+
+/**
+ *
+ * @author terck
+ */
+public class Resta {
+    public int resta(int num, int num2){
+    return num-num2;
+    }
+}
